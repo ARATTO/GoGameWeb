5f8ac6d6f85ad99b1410ebd279f538905002a774
Alam Lopez <alam.lqnpa6@hotmail.com>
2017-07-08 16:01:57 -0600

    Login, primer intento)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,32 +12,44 @@ class LoginController extends Controller
 {
     public function store(Requests $request)
     {
-        
 
         $usuario = User::Where('email',$request->email)->get()->toJson();
-        if($usuario!=null){
-        $usuariojson=json_decode($usuario);
-        
-        if(\Hash::check($request->password,$usuariojson[0]->password)){
-            if($usuariojson->IDESTUDIANTE!=NULL){
-                $numero=1;
-                return $numero;
 
-            } else if ($usuariojson->IDDOCENTE!=NULL){
-                $numero=2;
-                return $numero;
-            } else {
-                $numero=3;
+        if($usuario!=null){
+
+            $usuariojson=json_decode($usuario);
+        
+            if(\Hash::check($request->password,$usuariojson[0]->password)){
+
+                if($usuariojson->IDESTUDIANTE!=NULL){
+                    
+                    $numero=1;
+                    return $numero;
+
+                } else if ($usuariojson->IDDOCENTE!=NULL){
+
+                    $numero=2;
+                    return $numero;
+
+                } else {
+
+                    $numero=3;
+                    return $numero;
+
+                }
+            
+            }else {
+
+                $numero=4;
                 return $numero;
             }
+    
         }else {
-            $numero=4;
+            
+            $numero=5;
             return $numero;
+    
         }
-    }else {
-        $numero=5;
-        return $numero;
-    }
 
         
         
